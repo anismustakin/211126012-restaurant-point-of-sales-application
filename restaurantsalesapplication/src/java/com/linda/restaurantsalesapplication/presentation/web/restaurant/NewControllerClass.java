7ae00ddbf3e22b6a16f0e2076f66793a8802b8e7
linda@radian.co.za <linda@radian.co.za@ac3e4e38-e198-fd25-d4ad-fbb2b158a818>
2013-08-06 17:35:47 +0000

    Notes for Web services added

---
+++ restaurantsalesapplication/src/java/com/linda/restaurantsalesapplication/presentation/web/restaurant/NewControllerClass.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.linda.restaurantsalesapplication.presentation.web.restaurant;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author 211126012
+ */
+@Controller
+public class NewControllerClass {
+    
+    //Calculates total amounts
+    Facade data = new Facade();
+    //always return a string cause it represents the view
+    @RequestMapping("/amount.html") 
+    public String getTotalAmount(Model model)
+    {
+        //Chapter 8 in NOTES need web.xml file under public in WEB-INF
+        //must also still add the facade
+        //app config will have the servlet-context.xml - there is a line you need to change to reflect ur project
+        
+        List<Payment> payments = data.getPaymentCrudService().findAll();
+        BigDecimal amount = new BigDecimal("0.00");
+        //now can iterate through and add the amounts
+        for(Payment payment : payments)
+        {
+            amount = amount.add(payment.getAmount());
+        }
+        model.addAttributes("amount", amount);
+        return "amount/amount";
+    }
+}
