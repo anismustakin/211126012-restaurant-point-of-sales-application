b4a024dea8ee82f82fa9d4be6d5ff3d5fa3c00d0
lindavermeulen23@gmail.com <lindavermeulen23@gmail.com@ac3e4e38-e198-fd25-d4ad-fbb2b158a818>
2013-08-13 15:35:23 +0000

    Added web services

--- restaurantsalesapplication/test/com/linda/restaurantsalesapplication/test/crud/FoodItemCrudTest.java
+++ restaurantsalesapplication/test/com/linda/restaurantsalesapplication/test/crud/FoodItemCrudTest.java
@@ -10,6 +10,7 @@ import com.linda.restaurantsalesapplication.domain.FoodItem;
 import com.linda.restaurantsalesapplication.domain.Demographic;
 import com.linda.restaurantsalesapplication.domain.FoodItemContent;
 import com.linda.restaurantsalesapplication.domain.Name;
+import com.linda.restaurantsalesapplication.services.crud.FoodItemContentCrudService;
 import com.linda.restaurantsalesapplication.services.crud.FoodItemCrudService;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -32,6 +33,7 @@ public class FoodItemCrudTest {
     private static ApplicationContext ctx;
     private FoodItemCrudService foodItemCrudService;
     private Long id;
+    private FoodItemContentCrudService foodItemContentCrudService;
 
     public FoodItemCrudTest() {
     }
@@ -57,15 +59,11 @@ public class FoodItemCrudTest {
     private void createFoodItem() {
         // Create the Service Object
         foodItemCrudService = (FoodItemCrudService) ctx.getBean("foodItemCrudService");
+       
         
-        FoodItemContent content1 = OrderFactory.getFoodItemContent(5);
-        FoodItemContent content2 = OrderFactory.getFoodItemContent(6);
-        
-        List<FoodItemContent> contents = new ArrayList<FoodItemContent>();
-        
-        contents.add(content2);
-        contents.add(content1);
-        FoodItem foodItem = OrderFactory.getFoodItem("Burger", 12, 5, new BigDecimal("20.00"), contents);
+      
+       
+        FoodItem foodItem = OrderFactory.getFoodItem("Burger", 12, 5, new BigDecimal("20.00"));
         foodItemCrudService.persist(foodItem);
         //Collect the ID for use in later TESTS
         id = foodItem.getId();
