3b7e04f68048854970d2e79559c002f70198f914
lindavermeulen23@gmail.com <lindavermeulen23@gmail.com@ac3e4e38-e198-fd25-d4ad-fbb2b158a818>
2013-08-13 15:57:42 +0000

    Added web services

--- restaurantsalesapplication/src/java/com/linda/restaurantsalesapplication/presentation/web/restaurant/NewControllerClass.java
+++ restaurantsalesapplication/src/java/com/linda/restaurantsalesapplication/presentation/web/restaurant/NewControllerClass.java
@@ -4,8 +4,12 @@
  */
 package com.linda.restaurantsalesapplication.presentation.web.restaurant;
 
+import com.linda.restaurantsalesapplication.app.facade.Facade;
+import com.linda.restaurantsalesapplication.domain.FoodItem;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,14 +31,17 @@ public class NewControllerClass {
         //must also still add the facade
         //app config will have the servlet-context.xml - there is a line you need to change to reflect ur project
         //add.jsp shows the POST method and the University Controller shows how post works
-        List<Payment> payments = data.getPaymentCrudService().findAll();
-        BigDecimal amount = new BigDecimal("0.00");
-        //now can iterate through and add the amounts
-        for(Payment payment : payments)
-        {
-            amount = amount.add(payment.getAmount());
+         List<FoodItem> items = data.getFoodItemCrudService().findAll();
+         List<String> menu = new ArrayList<String>();
+         int count = 0;
+//        //now can iterate through and add the amounts
+         for(FoodItem item : items)
+         {
+             menu.add(item.getItemName());
+             count++;
         }
-        model.addAttributes("amount", amount);
-        return "amount/amount";
+        model.addAttribute("menus", menu);
+        model.addAttribute("count", count);
+        return "public/menu";
     }
 }
