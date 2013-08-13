66f228ed4f9fda0d9178c49764a4f0a3408b79f2
lindavermeulen23@gmail.com <lindavermeulen23@gmail.com@ac3e4e38-e198-fd25-d4ad-fbb2b158a818>
2013-08-13 15:34:11 +0000

    Added web services

--- restaurantsalesapplication/src/java/com/linda/restaurantsalesapplication/presentation/web/HomeController.java
+++ restaurantsalesapplication/src/java/com/linda/restaurantsalesapplication/presentation/web/HomeController.java
@@ -4,6 +4,16 @@
  */
 package com.linda.restaurantsalesapplication.presentation.web;
 
+import com.linda.restaurantsalesapplication.app.facade.Facade;
+import com.linda.restaurantsalesapplication.domain.Customer;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  *
  * @author 211126012
@@ -11,5 +21,35 @@ package com.linda.restaurantsalesapplication.presentation.web;
 public class HomeController 
 {
     private final Facade data = new Facade();
+     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
     
+    @RequestMapping({"/"})
+    public String home(Model model) {
+        
+        List<Customer> customers = data.getCustomerCrudService().findAll();
+        
+        model.addAttribute("customers", customers);
+
+        return "index";
+    }
+
+    @RequestMapping(value = "/about.html", method = RequestMethod.GET)
+    public String getAbout(Model model) {
+        return "about/about";
+    }
+    
+    @RequestMapping(value = "/contact.html", method = RequestMethod.GET)
+    public String getContactPage(Model model) {
+        return "public/contact";
+    }
+    
+    @RequestMapping(value = "/menu.html", method = RequestMethod.GET)
+    public String getMenu(Model model) {
+        return "public/menu";
+    }
+    
+    @RequestMapping(value = "/order.html", method = RequestMethod.GET)
+    public String getOrder(Model model) {
+        return "public/order";
+    }
 }
